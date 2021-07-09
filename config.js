7a6271b8c0b99edfed4b68be7ba2e98ff8f36a38
Baker Moran <baker.moran@joinroot.com>
2021-07-09 16:58:46 -0400

    adds base theme

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by Baker Moran',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Baker Moran',
+    photo: '/photo.jpg',
+    bio: 'Software Engineer.',
+    contacts: {
+      email: 'bamoran99@gmail.com',
+      facebook: '',
+      telegram: '',
+      twitter: 'https://twitter.com/bakermoran',
+      github: 'https://github.com/bakermoran',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'https://www.linkedin.com/in/baker-moran-a47b17114/',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
