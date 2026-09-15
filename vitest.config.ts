229359ed18cc96e8680f49b96b4f47b7f268a262
Baker Moran <52214859+bakermoran@users.noreply.github.com>
2026-09-14 21:54:30 -0700

    Add tests and coverage report with GHA (#14)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,26 @@
-import type {} from "vitest/config";
+import type { TestUserConfig } from "vitest/config";
 import { getViteConfig } from "astro/config";
 
-export default getViteConfig({
+type VitestConfig = Parameters<typeof getViteConfig>[0] & { test: TestUserConfig };
+
+const config: VitestConfig = {
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["@testing-library/jest-dom/vitest"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json-summary", "json", "lcov"],
+      reportOnFailure: true,
+      include: ["src/**"],
+      thresholds: {
+        lines: 14.13,
+        statements: 14.04,
+        functions: 9.25,
+        branches: 15.03,
+      },
+    },
   },
-});
+};
+
+export default getViteConfig(config);
