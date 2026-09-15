f488b2d69135cb733f5c7897af8b14a098012cac
Baker Moran <baker.moran@owner.com>
2026-09-14 21:18:36 -0700

    setup coverage and gha

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,17 @@ export default getViteConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["@testing-library/jest-dom/vitest"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json-summary", "json", "lcov"],
+      reportOnFailure: true,
+      include: ["src/**"],
+      thresholds: {
+        lines: 14.13,
+        statements: 14.04,
+        functions: 9.25,
+        branches: 15.03,
+      },
+    },
   },
 });
